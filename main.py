1c7f9a0685f42e4596732e45035b695c11a91f46
samuelsarzaba <samuelsarzaba@gmail.com>
2024-08-31 01:52:29 -0400

    Cleaned up imports

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from openpyxl import Workbook
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.alert import Alert
 import time
 from typing import Dict, List
+
+from openpyxl import Workbook
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
 quiz_link = ""
 
